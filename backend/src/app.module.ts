412bfcd1a37846558f0835a0a3b617f5b58c0033
adam.kent <adam.kent@thepetlabco.com>
2025-09-04 13:34:15 +0100

    remove boilerplate root controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { GameModule } from './modules/game/game.module';
-import { RootController } from './root.controller';
 
 @Module({
   imports: [GameModule],
-  controllers: [RootController],
+  controllers: [],
   providers: [PrismaService],
 })
 export class AppModule {}
--- backend/src/root.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller()
-export class RootController {
-  @Get()
-  hello() {
-    return 'Hello World!';
-  }
-}
